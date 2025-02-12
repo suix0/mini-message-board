b46b22a2328bbead6cacaa0b7250f8c07682c681
suix0 <112095627+suix0@users.noreply.github.com>
2025-02-12 18:51:31 +0800

    initialized express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server listening at port ${PORT}`));
